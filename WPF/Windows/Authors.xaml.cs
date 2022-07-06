7c76185c467d0e7ff4764c12743465eb4a3bb29c
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2022-07-06 20:02:27 +0200

    Authors window

---
+++ WPF/Windows/Authors.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPF.Entities;
+
+namespace WPF.Windows
+{
+    /// <summary>
+    /// Interaction logic for Authors.xaml
+    /// </summary>
+    public partial class Authors : Window
+    {
+        public Authors()
+        {
+            InitializeComponent();
+
+            LibraryEntities db = new LibraryEntities();
+            var authors = from a in db.Authors 
+                          select new { 
+                              FirstName = a.AFirstName,
+                              LastName = a.ALastName,
+                              FullName = a.AFullName
+                          };
+
+            this.gridAuthors.ItemsSource = authors.ToList();
+
+        }
+
+        private void addAuthorButton_Click(object sender, RoutedEventArgs e)
+        {
+            LibraryEntities db = new LibraryEntities();
+
+            Author author = new Author()
+            {
+                AFirstName = this.AuthorName.Text,
+                ALastName = this.AuthorLastName.Text,
+                AFullName = this.AuthorFullName.Text,
+            };
+
+            db.Authors.Add(author);
+            db.SaveChanges();
+        }
+
+        private void loadAuthors_Click(object sender, RoutedEventArgs e)
+        {
+            LibraryEntities db = new LibraryEntities();
+
+            var authors = from a in db.Authors
+                          select new
+                          {
+                              FirstName = a.AFirstName,
+                              LastName = a.ALastName,
+                              FullName = a.AFullName
+                          };
+
+            this.gridAuthors.ItemsSource = authors.ToList();
+        }
+    }
+}
